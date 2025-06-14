8ae57b08e0fcd0477ac41ae3c51ad1d969fc1453
sairajr62 <sairajr62@gmail.com>
2025-06-14 18:43:46 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-
-
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Caraousel from './components/Caraousel/caraousel'
@@ -16,18 +11,18 @@ function App() {
   
 
   return (
-    <Router>
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/About" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/Admissions" element={<Admissions/>} />
-      </Routes>
-      <Caraousel />
-      <Footer />
-    </>
-  </Router>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Caraousel />} />
+          <Route path="/About" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/admissions" element={<Admissions/>} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+  </>
 );
 }
 
