cee281d8634a6fef81b7a4ad359e51a8171a0cea
sairajr62 <sairajr62@gmail.com>
2025-06-14 18:48:15 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Caraousel from './components/Caraousel/caraousel'
+import Home from './components/Pages/Home';
 import About from './components/Pages/About';
 import Contact from './components/Pages/Contact';
 import Admissions from './components/Pages/Admissions';
---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Caraousel from '../Caraousel/Caraousel'
+
+const Home = () => {
+  return (
+    <div>
+        <Caraousel/>
+    </div>
+  )
+}
+
+export default Home
